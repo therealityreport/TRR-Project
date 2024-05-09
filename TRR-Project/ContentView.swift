88882620346699b553a274c78f1cb51bb7bb4477
TheRealityReport <admin@thereality.report>
2024-05-09 14:24:54 -0400

    Initial Commit

---
+++ TRR-Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TRR-Project
+//
+//  Created by thomas hulihan on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TRR-Project/TRR_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRR_ProjectApp.swift
+//  TRR-Project
+//
+//  Created by thomas hulihan on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TRR_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
